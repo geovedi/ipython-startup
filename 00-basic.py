5bcba7d51de86647d6b35c8b42412e3bc0a8b6e9
Jim Geovedi <jim@geovedi.com>
2015-01-23 11:52:44 +0700

    memory diagnostic

--- 00-basic.py
+++ 00-basic.py
@@ -1,10 +1,32 @@
 # -*- coding: utf-8 -*-
 
 import logging
-logging.basicConfig(format='%(asctime)s [%(process)d] [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s [%(process)d] [%(levelname)s] %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
 
 try:
     import regex as re
 except ImportError:
     import re
+
+
+# http://www.mobify.com/blog/sqlalchemy-memory-magic/
+from pympler import summary, muppy
+import psutil
+
+def get_virtual_memory_usage_kb():
+    """
+    The process's current virtual memory size in Kb, as a float.
+    """
+    return float(psutil.Process().memory_info_ex().vms) / 1024.0
+
+def memory_usage(where=None):
+    """
+    Print out a basic summary of memory usage.
+    """
+    mem_summary = summary.summarize(muppy.get_objects())
+    print "Memory summary:", where
+    summary.print_(mem_summary, limit=2)
+    print "VM: %.2fMb" % (get_virtual_memory_usage_kb() / 1024.0)
+
 
--- 02-ml.py
+++ 02-ml.py
@@ -6,31 +6,30 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
-from random import Random
-
 # stolen from @ogrisel Parallel ML tutorial
-class InfiniteStreamGenerator(object):
-    """Simulate random polarity queries on the twitter streaming API"""
-    
-    def __init__(self, texts, targets, seed=0, batchsize=100):
-        self.texts_pos = [text for text, target in zip(texts, targets)
-                               if target > 2]
-        self.texts_neg = [text for text, target in zip(texts, targets)
-                               if target <= 2]
-        self.rng = Random(seed)
-        self.batchsize = batchsize
-
-    def next_batch(self, batchsize=None):
-        batchsize = self.batchsize if batchsize is None else batchsize
-        texts, targets = [], []
-        for i in range(batchsize):
-            # Select the polarity randomly
-            target = self.rng.choice((0, 4))
-            targets.append(target)
-            
-            # Combine 2 random texts of the right polarity
-            pool = self.texts_pos if target > 2 else self.texts_neg
-            text = self.rng.choice(pool) + " " + self.rng.choice(pool)
-            texts.append(text)
-        return texts, targets
-
+#from random import Random
+#class InfiniteStreamGenerator(object):
+#    """Simulate random polarity queries on the twitter streaming API"""
+#    
+#    def __init__(self, texts, targets, seed=0, batchsize=100):
+#        self.texts_pos = [text for text, target in zip(texts, targets)
+#                               if target > 2]
+#        self.texts_neg = [text for text, target in zip(texts, targets)
+#                               if target <= 2]
+#        self.rng = Random(seed)
+#        self.batchsize = batchsize
+#
+#    def next_batch(self, batchsize=None):
+#        batchsize = self.batchsize if batchsize is None else batchsize
+#        texts, targets = [], []
+#        for i in range(batchsize):
+#            # Select the polarity randomly
+#            target = self.rng.choice((0, 4))
+#            targets.append(target)
+#            
+#            # Combine 2 random texts of the right polarity
+#            pool = self.texts_pos if target > 2 else self.texts_neg
+#            text = self.rng.choice(pool) + " " + self.rng.choice(pool)
+#            texts.append(text)
+#        return texts, targets
+#
